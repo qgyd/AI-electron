929329b3f68dd739ac29db0156db04cc5cc4c628
tph <2580190075@qq.com>
2026-04-20 18:42:35 +0800

    fix: 将 qiniu 和 axios 强制打入 main bundle 中，彻底解决 electron-builder 在打包时破坏其 node_modules 结构导致的运行时 Cannot find module 错误

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,9 +1,14 @@
 import { resolve } from 'path'
-import { defineConfig } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   main: {
+    plugins: [
+      externalizeDepsPlugin({
+        exclude: ['qiniu', 'axios'] // 强制打包 qiniu 和 axios，防止 electron-builder 破坏其 node_modules 结构
+      })
+    ],
     build: {
       rollupOptions: {
         external: ['sqlite3']
